9675daf99370780066f18716dd21b9d0210d1744
Aymeric Bouzigues <a.bouzigues@outlook.fr>
2020-04-16 18:21:54 +0200

    Added useful functions + started testing of model class

--- dataset/vocabulary_extractor.py
+++ dataset/vocabulary_extractor.py
@@ -1,5 +1,5 @@
 import pickle
-
+import numpy as np
 
 class VocabularyExtractor:
     def __init__(self, file_paths, then='auto'):
@@ -35,6 +35,9 @@ class VocabularyExtractor:
         with open('dataset/vocabulary_dictionary.pickle', 'wb') as f:
             pickle.dump(self.vocabulary_dictionary, f, pickle.HIGHEST_PROTOCOL)
 
+    def vocabulary_size(self):
+        return self.vocabulary_dictionary['total_count']
+
     def load_vocabulary_dictionary(self):
         try:
             with open('dataset/vocabulary_dictionary.pickle', 'rb') as f:
@@ -43,3 +46,17 @@ class VocabularyExtractor:
         except IOError:
             print("Warning : You tried to load the matrix but file vocabulary_dictionary.pickle is missing")
             return -1
+
+    def get_word_index(self, word):
+        if word in self.vocabulary_dictionary:
+            return self.vocabulary_dictionary[word]
+        else:
+            raise ValueError('Word not found in dictionary')
+
+    def get_context_vector(self, context: list) -> np.array:
+        out = np.zeros(self.vocabulary_size())
+        for word in context:
+            out[self.get_word_index(word)] += 1
+        return out/sum(out)
+
+
--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 import os
 import variables
 from dataset.vocabulary_extractor import VocabularyExtractor
+from model import Word2VecEmbeddingTrainer
+import numpy as np
 
 file_paths = ['dataset/raw/' + file_name for file_name in os.listdir(variables.dataset_path)]
 
 vocabulary_extractor = VocabularyExtractor(file_paths, then='create_dict')
+vocabulary_size = vocabulary_extractor.vocabulary_size()
 
+model = Word2VecEmbeddingTrainer(variables.word_dimension, vocabulary_size)
 
+test_sample = 'Participation in the study is'
+
+word_index = vocabulary_extractor.get_word_index('in')
+
+context = vocabulary_extractor.get_context_vector(['Participation', 'the', 'study'])
+
+predicted = model(word_index)
+print([predicted.shape])
+
+model.update_gradients_cross_entropy(predicted, context, word_index)
+model.backwards()
